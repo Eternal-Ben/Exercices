8a39e3de2efdde9cd7edd57c60bdb49555fc4c63
Ben.VisualStd <lst.thierry@gmail.com>
2019-05-08 17:33:11 +0200

    travail a faire sur le switch case et creatin de class à venir.

--- Test/HelloWord/HelloWord/Program.cs
+++ Test/HelloWord/HelloWord/Program.cs
@@ -1,15 +1,6 @@
-﻿/* Première application "Hello Word !"
-Utilisation des Methodes ;
-  Console.WriteLine(ce que l'on souhaite afficher) : permet l'affichage du résultat demandé à la console
-  Console.Readkey(...) & Console.ReadLine(...)     : permet à la console de recuperer 'la ligne || la touche' tapé par l'utilsateur
-*/
-
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
 //using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HelloWord
 {
@@ -17,11 +8,14 @@ namespace HelloWord
     {
         static void Main(string[] args)
         {
-            List<string> listUsers = new List<string> { "Alex", "Thierry", "Felipe" };
-            List<string> listKeys = new List<string> { "root", "toor", "admin" };
-            // A revoir sur la partie user et entre sur site.
-            bool users = listUsers<string>.Contains(users "Thierry");
-            bool keys = listKeys;
+            List<string> listUsers = new List<string> { "Alex", "Thierry", "Felipe" },
+                         listKeys = new List<string> { "root", "toor", "admin" };
+
+            bool key = Convert.ToBoolean(listKeys), // verif methode de convertsur bool et string ou convertion d'autre type...
+                 user = Convert.ToBoolean(listUsers),
+                 pass = key && user,
+                 quitter = "q" is "Q";
+
             var date = DateTime.Now;
 
             string aurevoir = "\nEn souhaitant vous revoir très prochainement, à tres bientôt !",
@@ -31,73 +25,76 @@ namespace HelloWord
                    mdp = Console.ReadLine(),
                    presentation = Console.ReadLine(),
                    personalKey = "root",
-                   nom = $"\n{date}\nVotre identifiant qui est votre prenom, je vous pries :",
+                   nom = $"\n{date}\nVous êtes dans un espace privé, veuillez préciser votre prénom je vous pries :",
                    bienvenu = $"\nBienvenu {presentation}, veuillez entrez votre mot de passe {date}:",
                    denicherb = "\nDennicher de bonnes affaire ? Oui tout de suite.",
                    souhait = $"\n\nQue souhaitez vous réaliser aujourd'hui {presentation}?",
                    interdiction = $"\n\aVous n'êtes pas autoriser à acceder à ce site, veuillez prendre contact avec notre administrateur, {administrateur}!";
+        }
+        string Message() // refac en class et recup les declarations d'objet adequat
+        {
+            var menu = new Menu();
+            Console.Clear();
+            Console.WriteLine($"{nom}", + $"{bienvenu}");
 
-            bool quitter = "q" is "Q";
+            if (Convert.ToBoolean(nom) != pass)
+            {
+                Console.WriteLine($"{interdiction}");
+                Console.WriteLine("");
+                Console.WriteLine($"\n{date}");
+                Console.ReadKey();
+            }
+            else
+            {
+                return menu;
+            }
+        }
 
+        string Menu() // refac en class
+        {
+                Console.Clear();
+                Console.Beep(3000, 90);
+                Console.Beep(3900, 90);
+                Console.Beep(4100, 190);
+                Console.WriteLine($"{bienvenu}");
 
+                Console.Clear();
+                Console.WriteLine($"{souhait}");
+                Console.WriteLine("1 . Visualiser : 1"); // faire une liste de consultation de client et de voyage en mode sql
+                Console.WriteLine("2 . Dénicher   : 2"); // idem ci dessus liste de voyage en dispo avec liste de client acheteur
+                Console.WriteLine("3 . Prospecter : 3"); // liste client potentiel et voyage dispo avec promo enfant 12 ans et liste tranche d'age etc via sql ?
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine(".qQ : Revenir un peux plus tard finalement... : Qq..");
+                Console.WriteLine("");
+                Console.WriteLine("");
 
-            Console.WriteLine($"{nom}");
+                int choixMenu = Console.Read();
+                switch (choixMenu)
+                {
+                    case 1:
+                        Console.WriteLine($"{armoir}");
+                        //new LeVoyage();
+                        Voyage.menu(); // attention au "static" dans la methode, menu.
+                                       /* var monobjet = new Voyage(); // instancier "minobjet" pour la creation */
+                        break;
+                    case 2:
+                        Console.WriteLine($"{denicherb}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"{peche}");
+                        break;
+                    case 4 when "q" is "Q":
 
-                        if (nom != user)
-                        {
-                            Console.Clear();
-                            Console.WriteLine($"{interdiction}");
-                            Console.WriteLine("");
-                            Console.WriteLine($"\n{date}");
-                            Console.ReadKey();
-                        }
-                        else
-                        {
+                    default:
+                        Console.WriteLine($"{aurevoir}");
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                        Console.WriteLine($"\n{date}");
+                        break;
+                }
+            }
 
-                            Console.Clear();
-                            Console.Beep(3000, 90);
-                            Console.Beep(3900, 90);
-                            Console.Beep(4100, 190);
-                            Console.WriteLine($"{bienvenu}");
-                        }
-                        {
-                            Console.Clear();
-                            Console.WriteLine($"{souhait}");
-                            Console.WriteLine("1 . Visualiser : 1"); // faire une liste de consultation de client et de voyage en mode sql
-                            Console.WriteLine("2 . Dénicher   : 2"); // idem ci dessus liste de voyage en dispo avec liste de client acheteur
-                            Console.WriteLine("3 . Prospecter : 3"); // liste client potentiel et voyage dispo avec promo enfant 12 ans et liste tranche d'age etc via sql ?
-                            Console.WriteLine("");
-                            Console.WriteLine("");
-                            Console.WriteLine(".qQ : Revenir un peux plus tard finalement... : Qq..");
-                            Console.WriteLine("");
-                            Console.WriteLine("");
-
-                                int choixMenu = Console.Read();
-                                switch (choixMenu)
-                                {
-                                    case 1:
-                                        Console.WriteLine($"{armoir}");
-									    //new LeVoyage();
-									    Voyage.menu(); // attention au "static" dans la methode, menu.
-                                                       /* var monobjet = new Voyage(); // instancier "minobjet" pour la creation */
-                                        break;
-                                case 2:
-                                        Console.WriteLine($"{denicherb}");
-                                        break;
-                                case 3:
-                                        Console.WriteLine($"{peche}");
-                                        break;
-                                case 4 when "q"is "Q":
-
-                                default:
-                                        Console.WriteLine($"{aurevoir}");
-                                        Console.WriteLine("");
-                                        Console.WriteLine("");
-                                        Console.WriteLine($"\n{date}");
-                                        break;
-                                }
-                        }
-         
         }
     }
 }
